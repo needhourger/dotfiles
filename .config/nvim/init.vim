242fc66c915d1fd3aa9e3b518e24fbf0d6a47de8
needhourger <chainyuuki@gmail.com>
2023-12-28 17:06:17 +0800

    add .config

---
+++ .config/nvim/init.vim
@@ -0,0 +1,27 @@
+syntax on
+filetype on
+filetype plugin on
+filetype indent on
+set nobackup
+setlocal noswapfile
+set bufhidden=hide
+set linespace=0
+set wildmenu
+set showmatch
+set nohlsearch
+
+set clipboard+=unnamedplus
+set number
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set noexpandtab
+set smartindent
+set autoindent
+set cindent
+set cursorline
+set ignorecase
+
+lua require('plugins')
+
+colorscheme tokyonight-night 
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,59 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- themes
+  use { "folke/tokyonight.nvim" }
+
+  use { 'nvim-lualine/lualine.nvim',
+	requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+	cond = [[not vim.g.vscode]] 
+  }
+
+  use  { 'neoclide/coc.nvim',
+	branch = "release",
+	cond = [[not vim.g.vscode]]
+  }
+
+  -- different color for different mode
+  use { 'mvllow/modes.nvim',
+	tag = 'v0.2.0',
+	config = function()
+		require('modes').setup({})
+	end,
+	cond = [[not vim.g.vscode]]
+  }
+
+  use { 'luukvbaal/statuscol.nvim',
+	config = function()
+		require("statuscol").setup({
+			setopt = true,
+		})
+	end,
+	cond = [[not vim.g.vscode]]
+  }
+
+  -- Special Cursor moving animation
+  use { 'gen740/SmoothCursor.nvim',
+	config = function()
+		require('smoothcursor').setup({
+			type="default",
+			fancy={
+				enable = true,
+			}
+		})
+	end,
+	cond = [[not vim.g.vscode]]
+  }
+
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "v2.*",
+    requires = 'kyazdani42/nvim-web-devicons',
+	cond = [[not vim.g.vscode]]
+  }
+end)

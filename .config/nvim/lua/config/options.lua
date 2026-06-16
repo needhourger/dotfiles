afe697fc417475c052d9f734e9ebd1abbe62cf13
needhourger <chainyuuki@gmail.com>
2026-06-16 17:40:52 +0800

    fix: zsh change from zsh-autosuggestions to zsh-autocomplete

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,6 @@ local g = vim.g
 opt.autoread = true
 opt.background = 'dark'
 opt.binary = true
-opt.clipboard = 'unnamedplus'
 opt.icon = true
 opt.lisp = true
 opt.mouse = 'a'
@@ -53,3 +52,38 @@ opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
 opt.cindent = true
+
+-- clipboard
+-- 1. 彻底关闭全局同步（防止 dd、x、c 影响系统）
+vim.opt.clipboard = ""
+
+-- 2. 依然显式指定 wl-clipboard 驱动
+vim.g.clipboard = {
+    name = 'wl-clipboard',
+    copy = {
+        ['+'] = 'wl-copy --type text/plain',
+        ['*'] = 'wl-copy --type text/plain'
+    },
+    paste = {
+        ['+'] = 'wl-paste --no-newline',
+        ['*'] = 'wl-paste --no-newline'
+    },
+    cache_enabled = 1
+}
+
+-- 利用neovim事件机制仅在y， yank时同步到系统剪切板
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Only sync to system clipboard when yanking",
+    callback = function()
+        -- vim.v.event.operator == "y" 确保只有 yank 操作触发，dd/x/c 等操作会被过滤掉
+        if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+            vim.fn.setreg("+", vim.v.event.regcontents)
+        end
+    end
+})
+
+-- 4. 粘贴优化：允许你在普通模式下直接通过 p 粘贴系统剪贴板的内容（可选）
+-- 如果不加这一行，你在 Neovim 里按 p 只能粘贴 Neovim 内部复制的东西
+vim.keymap.set('n', 'p', '"+p', {
+    desc = 'Paste from system clipboard'
+})
